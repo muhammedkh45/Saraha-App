06d8c68216af4a6bccaff05ad993b9d701991581
Muhammed Khalid <127145833+muhammedkh45@users.noreply.github.com>
2025-07-23 12:08:50 +0300

    Delete src directory

--- src/modules/users/users.controller.js
+++
@@ -1,9 +0,0 @@
-import Router from "express"
-import * as UC from "../users/users.service.js"
-const userRouter = Router()
-userRouter.post("/signup",UC.signUp)
-userRouter.post("/login",UC.login)
-userRouter.patch("",UC.updateLoggedInUser)
-userRouter.delete("",UC.deleteLoggedInUser)
-userRouter.get("",UC.getProfile)
-export default userRouter
